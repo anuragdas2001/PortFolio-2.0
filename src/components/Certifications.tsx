611d8194fed26e2296f29b820ddd65fbbddd8423
anuragdas2001 <anuragdas12921@gmail.com>
2024-04-14 12:02:36 +0530

    Moved certificates to public

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -46,35 +46,35 @@ export const Certifications = () => {
       content: <CertificationSkeleton />,
       className: "col-span-1",
       thumbnail:
-        "../../assets/certificates/certificate177307189ef448573c36cc9a17780c744db851b_page-0001.jpg",
+        "certificates/certificate177307189ef448573c36cc9a17780c744db851b_page-0001.jpg",
     },
     {
       id: 6,
       content: <CertificationSkeleton />,
       className: "col-span-1",
       thumbnail:
-        "../../assets/certificates/certificate1773072221c57f7625f24f4847367e55d9c9119_page-0001.jpg",
+        "certificates/certificate1773072221c57f7625f24f4847367e55d9c9119_page-0001.jpg",
     },
     {
       id: 7,
       content: <CertificationSkeleton />,
       className: "col-span-1",
       thumbnail:
-        "../../assets/certificates//certificate177307352b7ec1f2d066bc29385d38b3c54f7e4_page-0001.jpg",
+        "certificates/certificate177307352b7ec1f2d066bc29385d38b3c54f7e4_page-0001.jpg",
     },
     {
       id: 8,
       content: <CertificationSkeleton />,
       className: "col-span-1",
       thumbnail:
-        "../../assets/certificates/certificate28681851d8be6c7f7eb5277a8f4cdb3ae1e95b5_page-0001.jpg",
+        "certificates/certificate28681851d8be6c7f7eb5277a8f4cdb3ae1e95b5_page-0001.jpg",
     },
     {
       id: 9,
       content: <CertificationSkeleton />,
       className: "col-span-1",
       thumbnail:
-        "../../assets/certificates/certificate_16165b4507c9aeae_3c64142c22e5e9014b81e638fa887cb5_page-0001.jpg",
+        "certificates/certificate_16165b4507c9aeae_3c64142c22e5e9014b81e638fa887cb5_page-0001.jpg",
     },
   ];
   return (
