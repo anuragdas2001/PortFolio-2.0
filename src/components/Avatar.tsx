f9113bb1c829f6cbbc74ad695ed905464562fb89
anuragdas2001 <anuragdas12921@gmail.com>
2024-04-14 14:52:14 +0530

    Changed Avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,10 +2,10 @@ export const Avatar = () => {
   console.log("Avatar");
   return (
     <>
-      <div className="float-right items-end h-52 w-32 sm:h-60 sm:w-60 md:h-72 md:w-72 lg:h-96 lg:w-96   m-10 ">
+      <div className="float-right items-end -mt-5 me-8 ">
         <img
-          className="rounded-full "
-          src="https://pbs.twimg.com/profile_images/1778138976119037956/61OqPY9Q_400x400.jpg"
+          className="h-32 w-32 sm:h-60 sm:w-60 md:h-72 md:w-72 lg:h-96 lg:w-96  rounded-full"
+          src="profile/IMG_20240326_214004.jpg"
           alt="Image Description"
         />
       </div>
