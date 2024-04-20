00e6a723ea797516e3a42101dd1c48e724bcdf2d
anuragdas2001 <anuragdas12921@gmail.com>
2024-04-20 23:18:34 +0530

    Changed Avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -5,7 +5,7 @@ export const Avatar = () => {
       <div className="float-right items-end -mt-5 me-8 ">
         <img
           className="h-32 w-32 sm:h-60 sm:w-60 md:h-72 md:w-72 lg:h-96 lg:w-96  rounded-full"
-          src="profile/IMG_20240326_214004.jpg"
+          src="profile/661f6ede79c74.png"
           alt="Image Description"
         />
       </div>
